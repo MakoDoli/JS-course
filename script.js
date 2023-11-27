9aa16672afe1b48c3b8ecc661e2031ac3043ee85
MakoDoli <makodolidze@hotmail.com>
2023-11-27 13:11:34 +0400

    numbers are primitives

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-l;
+let num = 3;
+let newNum = 3.16;
+
+console.log(typeof num);
+console.log(typeof newNum);
+
+let mill = 1000000;
+let million = 1e6;
+// 1* (10**6) = 1000000
+console.log(million);
+
+let smallNum = 1e-6;
+console.log(smallNum);
+
+let score = new Number(100);
+console.log(typeof score);
+// no need to create number with 'new' keyword, javascript will do that for us
+console.log(score.toExponential());
