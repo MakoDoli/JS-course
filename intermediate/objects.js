63c5a4fbb24af048b0b8a9b5be0a43a78ed9f03a
MakoDoli <makodolidze@hotmail.com>
2023-12-12 14:37:06 +0400

    introducing objects

---
+++ intermediate/objects.js
@@ -0,0 +1,15 @@
+"use strict";
+const myObj = { name: "dave" };
+console.log(myObj);
+console.log(myObj.name);
+
+const anotherObj = {
+  alive: true,
+  answer: 42,
+  hobbies: ["eat", "sleep", "code"],
+  beverage: {
+    morning: "Coffee",
+    noon: "Lemonade",
+  },
+};
+console.log(anotherObj.alive);
