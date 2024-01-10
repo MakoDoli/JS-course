b8dba096dcff394c9c254a052c33a67347aef15f
MakoDoli <makodolidze@hotmail.com>
2024-01-10 23:55:56 +0400

    cookies flags

--- advanced/practice.js
+++ advanced/practice.js
@@ -317,3 +317,18 @@ const tree = {
   ],
 };
 printChildren(tree);
+
+// setting HTTPOny flag on cookies
+
+/*const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+app.use(cookieParser());
+app.get("/setcookie", (req, res) => {
+  res.cookie("myCookie", "eampleValue", { httpOnly: true });
+  res.send("Cookie set!");
+});
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});*/
